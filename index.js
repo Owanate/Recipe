d9647a9616bf63e972d5bb4778d4586bd2231324
Owanate <olifegeorge@gmail.com>
2022-06-07 16:44:20 +0100

    New update to add the modal

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let searchQuery = document.querySelector("input");
 const apiKey = "8dd45855b060473c83dc54dc67fb2869";
 let search = document.getElementById("search-btn");
 let viewBtn = document.getElementById("view-btn");
+let mealDetailsContent = document.querySelector(".meal-content-body");
+let recipeCloseBtn = document.querySelector(".recipe-close-btn");
 
 async function fetchAPI(e) {
     e.preventDefault();
@@ -16,12 +18,12 @@ async function fetchAPI(e) {
     }
   
     for(let i = 0; i < 6; i++){
-        let id = `${data.searchResults[4].results[i].id}`;
+        let id = `${data.searchResults[0].results[i].id}`;
         let recipe =
             `<div class="cards" id =${id}>
-              <img src="${data.searchResults[4].results[i].image}" alt="food-image">
-              <h2 class="title">${data.searchResults[4].results[i].name}</h2>
-              <a class="view-btn" target="_blank" href="${data.searchResults[4].results[i].link}">View Recipe</a>
+              <img src="${data.searchResults[0].results[i].image}" alt="food-image">
+              <h2 class="title">${data.searchResults[0].results[i].name}</h2>
+              <a class="view-btn" target="_blank">View Recipe</a>
             </div>`
             searchResultDiv.insertAdjacentHTML("afterbegin", recipe);
     }
@@ -30,6 +32,49 @@ async function fetchAPI(e) {
 search.addEventListener("click", fetchAPI);
 searchQuery.addEventListener("keyup", (e) => {
     if(e.key == "Enter"){
-        fetchAPI()
+        fetchAPI(e)
+        searchQuery.value = "";
     }
 })
+
+document.querySelector(".view-btn").addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
+
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('view-btn')){
+        let list = e.target.parentElement;
+        fetch(`https://api.spoonacular.com/recipes/${list.id}/information?apiKey=${apiKey}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data));
+    }
+}
+  
+  function mealRecipeModal(data){
+    let html =
+        `<h2 class="recipe-title"> ${data.title} </h2>
+        <div class="flex-container">
+          <div class="icons">
+            <i class='bx bxs-time' style='color:#645cff'></i>
+            <p>PrepTime</p>
+            <p>${data.readyInMinutes}</p>
+          </div>
+          <div class="icons">
+            <i class='bx bxs-badge-check' style='color:#645cff'></i>
+            <p>Health Score</p>
+            <p>${data.healthScore}</p>
+          </div>
+          <div class="icons">
+            <i class='bx bx-group' style='color:#645cff'></i>
+            <p>Serving</p>
+            <p>${data.servings}</p>
+          </div>
+         </div>
+         <p>Wine Pairings: ${data.winePairing.pairingText}</p>
+         <a class="recipe-link" href=${data.sourceUrl}>Get the full Recipe</a>
+        </div>`
+        mealDetailsContent.innerHTML = html;
+        mealDetailsContent.parentElement.classList.add('showRecipe');
+}
